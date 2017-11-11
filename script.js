e4cd301fe3df19558470f3410053f6ad7b872dcb
Guillermo Olivos <gcolivos@gmail.com>
2017-11-11 13:45:38 -0600

    Finished Base

---
+++ script.js
@@ -0,0 +1,47 @@
+console.log("In script.js");
+
+
+
+$(document).ready(onReady);
+
+    // $('.container').append('<button id="submitButton">Submit!</button>');
+    // $('#submitButton').on('click', compareNumbers)
+
+
+
+function onReady(){
+    console.log("Ready to roll");
+    $('#submitButton').on('click', addEmployee)
+}
+
+var employeeList=[];
+var totalCost=0;
+
+function addEmployee(e, complete){
+    console.log("In addEmployee")
+    employeeList.push({
+        firstName: $('#firstName').val(),
+        lastName: $('#lastName').val(),
+        idNumber: $('#idNumber').val(),
+        jobTitle: $('#jobTitle').val(),
+        annualSalary: $('#annualSalary').val()
+    })
+    totalCost+= Number($('#annualSalary').val());
+    $("#annualCost").replaceWith("<p id='annualCost'> Annual Company Cost: "+ totalCost + "</p>");
+    $('#salaryTBody').append(
+        "<tr>" +
+        "<td>"+ employeeList[employeeList.length-1].firstName +"</td>" +
+        "<td>"+ employeeList[employeeList.length-1].lastName +"</td>" +
+        "<td>"+ employeeList[employeeList.length-1].idNumber +"</td>" +
+        "<td>"+ employeeList[employeeList.length-1].jobTitle +"</td>" +
+        "<td>"+ employeeList[employeeList.length-1].annualSalary +"</td>"        
+    )
+    document.getElementById("firstName").value = ""
+    document.getElementById("lastName").value = ""
+    document.getElementById("idNumber").value = ""
+    document.getElementById("jobTitle").value = ""
+    document.getElementById("annualSalary").value = ""
+
+    console.log(employeeList[employeeList.length-1]);
+}
+
